45bd6ff4e1c6eb2f70eb027177bd285615d66684
sdasd <vpaira@gmail.com>
2024-11-07 01:29:49 +0530

    dotenv

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-
+from dotenv import load_dotenv
+import uvicorn
+import os
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+load_dotenv()
+if os.getenv('uvicorn') == "1":
+    if __name__ == "__main__":
+        uvicorn.run("main:app", port=8080, host="127.0.0.1", reload=True)
